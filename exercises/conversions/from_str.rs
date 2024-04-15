75590d853b670e9e8910314f538eb374a86850cd
gph <1598542650@qq.com>
2024-04-15 15:01:53 +0800

    update

--- exercises/conversions/from_str.rs
+++ exercises/conversions/from_str.rs
@@ -31,7 +31,6 @@ enum ParsePersonError {
     ParseInt(ParseIntError),
 }
 
-// I AM NOT DONE
 
 // Steps:
 // 1. If the length of the provided string is 0, an error should be returned
@@ -52,6 +51,34 @@ enum ParsePersonError {
 impl FromStr for Person {
     type Err = ParsePersonError;
     fn from_str(s: &str) -> Result<Person, Self::Err> {
+        if s.len() == 0 {
+            return Err(ParsePersonError::Empty);
+        }
+
+        match s.split_once(',') {
+            Some((name,age)) => {
+                if name.is_empty() {
+                    return Err(ParsePersonError::NoName);
+                }else if age.contains(',') {
+                    return Err(ParsePersonError::BadLen);
+                }else {
+                    match age.parse::<usize>() {
+                        Ok(a) => {
+                            return Ok(Person {
+                                name:name.to_owned(),
+                                age:a,
+                            })
+                        },
+                        Err(a) => {
+                            return Err(ParsePersonError::ParseInt(a));
+                        },
+                    }
+                }
+            },
+            _ => {
+                return Err(ParsePersonError::BadLen);
+            },
+        }
     }
 }
 
